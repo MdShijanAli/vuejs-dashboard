42776243eeb0665fcf94bc5b0a6fd052d255a6fa
MdShijanAli <freelancershijan@gmail.com>
2024-04-11 06:37:33 +0600

    refactor: update the design

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import router from './router';
 
 import PrimeVue from 'primevue/config';
+import InputSwitch from 'primevue/inputswitch';
+import SelectButton from 'primevue/selectbutton';
 import Sidebar from 'primevue/sidebar';
 
 import '@/assets/styles.scss';
@@ -12,6 +14,7 @@ const app = createApp(App);
 app.use(router);
 app.use(PrimeVue, { ripple: true });
 
+app.component('InputSwitch', InputSwitch);
+app.component('SelectButton', SelectButton);
 app.component('Sidebar', Sidebar);
-
 app.mount('#app');
